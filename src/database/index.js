df26b748b0d103358e094abb2c366cd5513906c7
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-01-19 18:41:35 -0300

    Appointment model and migration

--- src/database/index.js
+++ src/database/index.js
@@ -2,26 +2,24 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
-    constructor() {
-        this.init();
-    }
+  constructor() {
+    this.init();
+  }
 
-    init() {
-        this.connection = new Sequelize(databaseConfig);
+  init() {
+    this.connection = new Sequelize(databaseConfig);
 
-        models
-            .map(model => model.init(this.connection))
-            .map(
-                model =>
-                    model.associate && model.associate(this.connection.models)
-            );
-    }
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
 }
 
 export default new Database();
