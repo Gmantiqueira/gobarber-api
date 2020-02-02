c9e93ae539991cd2f8cfe954141532c43a981a55
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-02-02 19:50:01 -0300

    WIP | Available hours controller

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
+import AvailableController from './app/controllers/AvailableController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 
@@ -23,6 +24,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
