b18a23869f2143baee4a58b279a683e90d174ef6
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-02-09 15:32:03 -0300

    Configurações avançadas

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'bcba9795f0544e',
-    pass: 'ae4cbf0d163ab5',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
